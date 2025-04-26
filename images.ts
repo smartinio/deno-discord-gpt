d208fae3acab690892da42e6a37536739f41e0c0
Sam Martin <me@smartin.io>
2025-04-26 18:29:58 +0200

    Edit images

--- images.ts
+++ images.ts
@@ -27,13 +27,21 @@ export const resolveImage = async (url: string) => {
   return btoa(binaryString);
 };
 
+export const resolveImageAsFile = async (url: string) => {
+  const imageBase64 = await resolveImage(url);
+
+  return new File([atob(imageBase64)], "image.jpeg", {
+    type: "image/jpeg",
+  });
+};
+
 const baseUrl = Deno.env.get("BASE_URL") || "http://localhost:8000";
 
 export const saveGeneratedImage = async (b64: string) => {
   const id = crypto.randomUUID() + ".jpeg";
 
   await redis.set(`generated-image:${id}`, b64, {
-    ex: 60 * 60 * 24, /* 1 day */
+    ex: 60 * 60 * 12, /* 12h */
   });
 
   return `${baseUrl}/generated-image/${id}`;
